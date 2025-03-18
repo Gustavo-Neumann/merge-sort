578cdc8ca69a937c1038c1e153ddb592d06c9d00
Gustavo Neumann <gneumann7164@gmail.com>
2025-03-17 22:54:51 -0300

    fix upload only go files

--- c/iterative.h
+++
@@ -1,6 +0,0 @@
-#ifndef ITERATIVE_H
-#define ITERATIVE_H
-
-void iterativeMergeSort(int arr[], int n);
-
-#endif /* ITERATIVE_H */
--- c/recursive.h
+++
@@ -1,6 +0,0 @@
-#ifndef RECURSIVE_H
-#define RECURSIVE_H
-
-void recursiveMergeSort(int arr[], int n);
-
-#endif /* RECURSIVE_H */
